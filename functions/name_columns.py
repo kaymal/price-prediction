6e7937d952482364998fada476916627ae37c0dd
kaymal <gutkyle@gmail.com>
2019-06-17 21:03:30 +0200

    Create name_columns.py

---
+++ functions/name_columns.py
@@ -0,0 +1,20 @@
+def name_columns(data):
+    '''
+    Name columns with underscore(_) convention.
+    '''
+    # Clean "\n"s from the column names
+    data.columns = data.columns.str.strip("\n")
+
+    # Make lowercase
+    data.columns = data.columns.str.lower()
+    
+    # Replace space with underscore(_)
+    data.columns = data.columns.str.replace(" ", "_")
+    
+    # Replace . with ""
+    data.columns = data.columns.str.replace(".", "")
+    
+    # Replace "_&_" with "&"
+    data.columns = data.columns.str.replace("_&_", "&")
+    
+    return data.columns
